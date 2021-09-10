4ceb7d0d177ee8e57b0db2428edd4771cc5bbd79
Cyril <cyril.dumont@leuville.com>
2021-09-10 14:27:25 +0100

    Registry docker

---
+++ src/main/java/fr/orsys/msa/gc/registry/GcRegistryApplication.java
@@ -0,0 +1,15 @@
+package fr.orsys.msa.gc.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class GcRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/orsys/msa/gc/registry/GcRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orsys.msa.gc.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
